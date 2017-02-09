389cec6f8853a4f83af6270e4629a277feaa8b8f
Daniel Huth <dannyfore@web.de>
2017-02-09 12:13:26 +0100

    Camera follow

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,16 +1,73 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+namespace UnityStandardAssets.Utility
+{
+	public class CameraController : MonoBehaviour
+	{
 
-	// Use this for initialization
-	void Start () {
+		// The target we are following
+		[SerializeField]
+		private List<Transform> targets;
+		// The distance in the x-z plane to the target
+		[SerializeField]
+		private float distance = 10.0f;
+		// the height we want the camera to be above the target
+		[SerializeField]
+		private float height = 1.0f;
+
+		[SerializeField]
+		private float heightDamping;
+
+		[SerializeField]
+		private float minHeight = 2f;
 		
-	}
+		[SerializeField]
+		private float maxHeight = 8f;
+
+		// TODO: Debug
+		[SerializeField]
+		private GameObject centerObject;
 	
-	// Update is called once per frame
-	void Update () {
-		
+		// Use this for initialization
+		void Start() { 		
+		}
+
+		// Update is called once per frame
+		void LateUpdate()
+		{
+			if(targets.Count < 1)
+				return;
+				
+				
+			// Find Center of Objects	
+			var center = new Vector3(0,0,0);	
+			
+			foreach(Transform _target in targets) {
+				center += _target.position;
+			}
+				
+			var target = centerObject.transform;
+
+			target.position = center / targets.Count;
+
+			// Calculate Height
+			
+			var centerDist = Vector3.Distance(targets[0].position, target.position);
+			
+			var wantedHeight = target.position.y + height + (centerDist*1.8f);
+			
+
+			// Set the position of the camera on the x-z plane to:
+			// distance meters behind the target
+			transform.position = target.position - (Vector3.forward * distance);
+
+			// Set the height of the camera
+			transform.position = new Vector3(transform.position.x ,wantedHeight , transform.position.z);
+
+			// Always look at the target
+			transform.LookAt(target);
+			//transform.rotation =  Quaternion.Euler(69, 0, 0);
+		}
 	}
 }
