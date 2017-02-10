41d3c06815b75871f2383dcbd1f539d38b613419
Daniel Huth <dannyfore@web.de>
2017-02-10 12:24:55 +0100

    Added theft

---
+++ Assets/Scripts/Theft.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Theft : Character {
+
+	/**
+		TOOD: 
+			+ Krystall
+			+ Trap
+			+ 
+	
+	**/
+	
+	private Animator m_Animator;
+	
+	public void Start(){
+		m_Animator = GetComponent<Animator>();
+	}
+	
+	
+	public void Kill() {
+		// Start Animation
+	}
+	
+	/**
+		
+	**/
+	public override void OnColissionEnter(Collision collision){
+	
+	}
+	
+	/**
+		TODO: Lay Trap
+	**/
+	public override void Action(){
+		
+	}
+	
+	
+
+
+}
